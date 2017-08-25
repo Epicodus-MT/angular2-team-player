453a9d927016e682625f8ba12aeb7a996358d62e
Mara Timberlake <maratimberlake@msn.com>
2017-08-25 14:24:32 -0700

    add player-detail component, mock-player.ts, and player.service.ts files

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,28 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Player } from '../player.model';
 import { Router } from '@angular/router'
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-roster',
   templateUrl: './roster.component.html',
   styleUrls: ['./roster.component.css']
+  providers: [PlayerService]
 })
-export class RosterComponent {
-  constructor(private router: Router){
 
+export class RosterComponent implements OnInit {
+  players: Player[] = [];
+
+  constructor(private router: Router, private playerService: PlayerService) {}
+
+  ngOnInit(){
+    this.players = this.playerService.getPlayers();
   }
-  
-  players: Player[] = [
-   new Player("Henry Wingo", 23, "Midfielder", 21, "Lake Forest Park, WA" 1),
-
-  new Player("Osvaldo Alonso", 6, "Midfielder", 31, "an Cristóbal, Cuba" 2),
-
-  new Player("Brad Evans", 3, "Defender/Midfielder", 32, "San Cristóbal, Cuba" 3),
-
-  new Player("Kelvin Leerdam", 18, "Defender", 27, "Utrecht, Netherlands" 4),
-
-  new Player("Nouhou Tolo", 5, "Defender", 20, "Douala, Cameroon" 5),
- ];
 
   goToDetailPage(clickedPlayer: Player) {
     this.router.navigate(['players', clickedPlayer.id]);
